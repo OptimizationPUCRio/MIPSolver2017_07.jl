4802eb2771ec5490b1c5a2cd8c70e986b0865ec7
RodrigoVillas <31329321+RodrigoVillas@users.noreply.github.com>
2017-11-15 12:21:14 -0200

    Update BranchNbound.jl

--- BranchNbound.jl
+++ BranchNbound.jl
@@ -1,241 +1,268 @@
-sing JuMP
+using JuMP
 using Cbc
-
 start=time()
 
-# Estruturas usadas no Branck N Bound
-
-  #Definição da direção do problema
-  if getobjectivesense(m) == :Max
-
-    direcao = 1
-
-    ZglobalINT = -Inf
-
-    Zbound = Inf
-
-  else
-
-    direcao = 2
-
-    ZglobalINT = Inf
-
-    Zbound = -Inf
-
-  end
-  #---------------------------
-
-  #Estruta do nó
-    mutable struct node
-
-        model::JuMP.Model
-
-    end
-  #---------------------------
-
-  #Estruta da lista
-
-  vectorIndex=Vector{Int}(0)
-
-  for i = 1:length(m.colCat) #PROCURA UMA VARIÁVEL BINÁRIA
-
-    if m.colCat[i] == :Bin
-
-    push!(vectorIndex,i)
-
-    end
-
-  end
-
-  lista = Vector{node}(0)
-
-  model=deepcopy(m)
-
-  push!(lista,node(model))
-
-  #---------------------------
-
-  #Variáveis utilizadas como Flags ou saídas do problema
-
-  k=0
-
-  flag=0
-
-  Solu=0
-
-  bestx=0
-
-  maxit=1
-
-  iter = 0
-  #-------------------------------
+    # Estruturas usadas no Branck N Bound
 
 
-  #BRANCH N Bound
+      #Definição da direção do problema
+      if getobjectivesense(m) == :Max 
 
-  while abs(Zbound-ZglobalINT) > exp10(-3) && iter <= maxit
+        direcao = 1
 
-    flag=0
+        ZglobalINT = -Inf
 
-    model=lista[1].model
+        Zbound = Inf
 
-    status= solve(model, relaxation = true)
+        Zbound2 = Inf
+      else
 
-    if direcao == 1
+        direcao = 2
 
-        if getobjectivevalue(model) < Zbound
+        ZglobalINT = Inf
 
-          Zbound = getobjectivevalue(model)
+        Zbound = -Inf
 
+        Zbound2 = -Inf
+      end
+      #---------------------------
+
+      #Estruta do nó
+        mutable struct node
+
+            model::JuMP.Model
+
+            Level::Integer
+            
         end
-    else
+      #---------------------------
 
-        if getobjectivevalue(model) > Zbound
+      #Estruta da lista
 
-          Zbound = getobjectivevalue(model)
+      vectorIndex=Vector{Int}(0)
 
-        end
+      for i = 1:length(m.colCat) #PROCURA UMA VARIÁVEL BINÁRIA
 
-    end
+        if m.colCat[i] == :Bin
 
-    println("LB = ", getobjectivevalue(model))
-
-    #PODA POR VIABILIDADE
-
-    if status!=:Optimal
-
-      shift!(lista)
-
-      flag=1
-
-    end
-
-    if flag==0
-
-
-      if sum(model.colVal[vectorIndex])-sum(round.(model.colVal[vectorIndex])) == 0  #PODA POR OTIMALIDADE
-
-        Solu=1
-
-        if direcao == 1
-
-          if getobjectivevalue(model) > ZglobalINT
-
-            ZglobalINT = getobjectivevalue(model)
-
-            bestx=model.colVal[:]
-
-          end
-
-        else
-
-          if getobjectivevalue(model) < ZglobalINT
-
-            ZglobalINT = getobjectivevalue(model)
-
-            bestx=model.colVal[:]
-
-          end
-
-        end
-
-        shift!(lista)
-
-        flag=3
-
-      else #PODA POR LIMITE
-
-        if direcao == 1
-
-          if getobjectivevalue(model) < ZglobalINT
-
-            shift!(lista)
-
-            flag =2
-
-          end
-
-        else
-
-          if getobjectivevalue(model) > ZglobalINT
-
-            shift!(lista)
-
-            flag =2
-
-          end
+        push!(vectorIndex,i)
 
         end
 
       end
 
-      if flag==0
+      lista = Vector{node}(0)
 
-        j=0
+      model=deepcopy(m)
 
-        for i in vectorIndex #Ve se a variáevl é N BIn
+      push!(lista,node(model,0))
 
-          if model.colVal[i]-round(model.colVal[i]) !=0
+      #---------------------------
 
-                j=i
+      #Variáveis utilizadas como Flags ou saídas do problema
+
+      Level=0
+      
+      k=0
+
+      flag=0
+
+      Solu=0
+
+      bestx=0
+
+      maxit=20
+
+      iter = 0
+
+      #-------------------------------
+
+
+      #BRANCH N Bound
+
+      while abs(Zbound2-ZglobalINT) > exp10(-3) && iter <= maxit && length(lista)>0
+
+        flag=0
+
+        model=lista[1].model  
+          
+        status= solve(model, relaxation = true)
+
+        if direcao == 1
+
+            if getobjectivevalue(model) < Zbound && lista[1].Level == Level
+
+              Zbound = getobjectivevalue(model)
+
+            elseif  lista[1].Level != Level
+
+              Zbound2 = Zbound
+
+              Zbound =Inf
+              
+              Level= Level +1
+                
+            end
+        else
+
+            if getobjectivevalue(model) > Zbound && lista[1].Level == Level
+
+              Zbound = getobjectivevalue(model)
+
+            elseif  lista[1].Level != Level
+
+              Zbound2 = Zbound
+
+              Zbound =-Inf
+                
+              Level= Level +1
+              
+            end
+
+        end
+
+        println("LB = ", getobjectivevalue(model))
+
+        #PODA POR VIABILIDADE
+
+        if status!=:Optimal
+
+          shift!(lista)
+
+          flag=1
+
+        end
+
+        if flag==0
+          println("xx == ", model.colVal[vectorIndex])
+
+          if sum(model.colVal[vectorIndex])-sum(round.(model.colVal[vectorIndex])) <= 0.01  #PODA POR OTIMALIDADE
+
+            Solu=1
+
+            if direcao == 1
+
+              if getobjectivevalue(model) > ZglobalINT
+
+                ZglobalINT = getobjectivevalue(model)
+
+                bestx=model.colVal[:]
+
+              end
+
+            else
+
+              if getobjectivevalue(model) < ZglobalINT
+
+                ZglobalINT = getobjectivevalue(model)
+
+                bestx=model.colVal[:]
+
+              end
+
+            end
+
+            shift!(lista)
+
+            flag=3
+
+          else #PODA POR LIMITE
+
+            if direcao == 1
+
+              if getobjectivevalue(model) < ZglobalINT
+
+                shift!(lista)
+
+                flag =2
+
+              end
+
+            else
+
+              if getobjectivevalue(model) > ZglobalINT
+
+                shift!(lista)
+
+                flag =2
+
+              end
+
+            end
+
+          end
+
+          if flag==0
+
+            j=0
+
+            for i in vectorIndex #Ve se a variáevl é N BIn
+
+              if model.colVal[i]-round(model.colVal[i]) !=0
+
+                    j=i
+
+              end
+
+            end
+
+
+            #PRIMEIRO BRANCH
+
+            modelRight=deepcopy(model)
+            
+            modelRight.colLower[j]=floor(modelRight.colVal[j])     #floor(modelRight.colVal[j])
+          
+            modelRight.colUpper[j]=floor(modelRight.colVal[j])     #floor(modelRight.colVal[j])
+      
+            NodeR=node(modelRight,lista[1].Level+1)
+
+            push!(lista,NodeR)
+
+            #-------------------------
+
+
+            #SEGUNDO BRANCH
+
+            modelLeft=deepcopy(model)
+            
+            modelLeft.colLower[j]=ceil(modelLeft.colVal[j])     #ceil(modelLeft.colVal[j])
+
+            modelLeft.colUpper[j]=ceil(modelLeft.colVal[j])      #ceil(modelLeft.colVal[j])
+
+            NodeL=node(modelLeft,lista[1].Level+1)
+            
+            push!(lista,NodeL)
+
+            #-------------------------
+
+            shift!(lista)
 
           end
 
         end
 
-        shift!(lista)
-
-        #PRIMEIRO BRANCH
-
-        modelLeft=deepcopy(model)
-
-        modelLeft.colLower[j]=ceil(modelLeft.colVal[j])     #ceil(modelLeft.colVal[j])
-
-        modelLeft.colUpper[j]=ceil(modelLeft.colVal[j])      #ceil(modelLeft.colVal[j])
-
-        NodeL=node(modelLeft)
-
-        push!(lista,NodeL)
-
-        #-------------------------
-
-
-        #SEGUNDO BRANCH
-
-        modelRight=deepcopy(model)
-
-        modelRight.colLower[j]=floor(modelRight.colVal[j])     #floor(modelRight.colVal[j])
-
-        modelRight.colUpper[j]=floor(modelRight.colVal[j])     #floor(modelRight.colVal[j])
-
-        NodeR=node(modelRight)
-
-        push!(lista,NodeR)
-
-        #-------------------------
-
-
+        iter=iter+1
       end
 
-    end
-    println("Z convergence = ", ZglobalINT)
-    iter=iter+1
-  end
+      fim =time()
 
-  fim =time()
+      if Solu == 1
 
-  if Solu == 1
+        m.colVal=bestx
 
-    m.colVal=bestx
+        println("Iterações= ", iter)
 
-    println("Z convergence = ", ZglobalINT)
+        println("Z convergence = ", ZglobalINT)
 
-    println("tempo = ", fim-start)
+        println("tempo = ", fim-start)
 
-  else
+      else
 
-    println("Inveasible")
+        println("Iterações= ", iter)
 
-    println("tempo = ", fim-start)
+        println("Inveasible")
 
-  end
+        println("tempo = ", fim-start)
+
+      end
